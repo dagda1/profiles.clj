6a3d9f4e75ed54d0bb57ce79209aed6c1798a7dc
dagda1 <dagda1@scotalt.net>
2015-07-23 21:45:08 +0100

    update cider

--- profiles.clj
+++ profiles.clj
@@ -1,19 +1,5 @@
 {:user {
-        :dependencies [[com.cemerick/piggieback "0.1.4"]]
-        :plugins [
-                  [cider/cider-nrepl "0.9.0-SNAPSHOT"]
-                  ]
-        :repl-options {:nrepl-middleware
-                 [cider.nrepl.middleware.apropos/wrap-apropos
-                  cider.nrepl.middleware.classpath/wrap-classpath
-                  cider.nrepl.middleware.complete/wrap-complete
-                  cider.nrepl.middleware.info/wrap-info
-                  cider.nrepl.middleware.inspect/wrap-inspect
-                  cider.nrepl.middleware.macroexpand/wrap-macroexpand
-                  cider.nrepl.middleware.ns/wrap-ns
-                  cider.nrepl.middleware.resource/wrap-resource
-                  cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                  cider.nrepl.middleware.test/wrap-test
-                  cider.nrepl.middleware.trace/wrap-trace
-                  cider.nrepl.middleware.undef/wrap-undef]}
+        :dependencies [[com.cemerick/piggieback "0.2.1"]
+                       [org.clojure/tools.nrepl "0.2.10"]]
+        :plugins [[cider/cider-nrepl "0.10.0-SNAPSHOT"]]
         }}
